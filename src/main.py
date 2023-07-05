d366771779836cc8659a926bb7eb71f5ce9ac057
kamil <kamil.taleb.1@ens.etsmtl.ca>
2023-07-05 16:04:57 -0400

    mauvais commit ne devrait pas passer

--- src/main.py
+++ src/main.py
@@ -82,6 +82,7 @@ class Main:
         except requests.exceptions.RequestException as e:
             # To implement
             pass 
+        
     def badly_formatted_function(): return "Badly formatted code"
 
 
